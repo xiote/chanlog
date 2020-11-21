b39b54716a03c8295fa7b9629f6254d0b51438d4
xiote <xiote@nate.com>
2020-11-22 00:31:32 +0900

    modified:   chanlog.go 	modified:   chanlog_test.go

--- chanlog.go
+++ chanlog.go
@@ -1,13 +1,37 @@
 package chanlog
 
 import (
+	"fmt"
 	"log"
 )
 
+var logchan chan string
+
+func init() {
+	logchan = make(chan string, 1000)
+
+	go func() {
+		// 채널이 닫힐 때까지 메시지 받으면 로깅
+		for msg := range logchan {
+			log.Print(msg)
+		}
+	}()
+}
+
+func SetChanSize(size int) {
+	logchan = make(chan string, size)
+}
+
+func SetFlags(flag int) {
+	log.SetFlags(flag)
+}
+
 func Print(v ...interface{}) {
-	log.Print(v...)
+	// log.Printf(v...)
+	logchan <- fmt.Sprint(v...)
 }
 
 func Printf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	// log.Printf(format, v...)
+	logchan <- fmt.Sprintf(format, v...)
 }
--- chanlog_test.go
+++ chanlog_test.go
@@ -4,12 +4,18 @@ import (
 	// "fmt"
 	// "github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
+func init() {
+	SetChanSize(10)
+}
+
 func TestPrint(t *testing.T) {
-	Print("HelloWorld")
+	Print("TestPrint")
+	time.Sleep(time.Second)
 }
 
 func TestPrintf(t *testing.T) {
-	Printf("%s", "HelloWorld")
+	Printf("%s", "TestPrintf")
 }
